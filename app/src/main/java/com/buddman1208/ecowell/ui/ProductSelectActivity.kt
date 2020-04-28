140dcea837639c1fa5da127a903bd28d3e6b88d8
Junseok Oh <buddman1208@gmail.com>
2020-04-28 17:03:12 +0900

    [product select] init

---
+++ app/src/main/java/com/buddman1208/ecowell/ui/ProductSelectActivity.kt
@@ -0,0 +1,13 @@
+package com.buddman1208.ecowell.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.buddman1208.ecowell.R
+
+class ProductSelectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
