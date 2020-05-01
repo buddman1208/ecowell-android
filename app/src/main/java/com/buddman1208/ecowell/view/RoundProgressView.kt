0ff04d830e04f34735acebaa6f388157aa8cd635
Junseok Oh <buddman1208@gmail.com>
2020-05-01 16:50:51 +0900

    [roundprogressview] apply default padding, margin and stroke color

--- app/src/main/java/com/buddman1208/ecowell/view/RoundProgressView.kt
+++ app/src/main/java/com/buddman1208/ecowell/view/RoundProgressView.kt
@@ -1,32 +1,88 @@
 package com.buddman1208.ecowell.view
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.buddman1208.ecowell.R
 
+
 class RoundProgressView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    private var progressWidth : Int = 10
-    private var progressColor : Int = ContextCompat.getColor(context, R.color.colorPrimary)
-    private var centerColor : Int = Color.WHITE
+    // Config
+
+    // View Options
+    private var strokeWidth: Int = 10
+    private var strokeColor: Int = Color.parseColor("#EEEEEE")
+    private var progressColor: Int = ContextCompat.getColor(context, R.color.colorAccent)
+
+    private val progressPaint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = progressColor
+            style = Paint.Style.STROKE
+            strokeWidth = this@RoundProgressView.strokeWidth.toFloat()
+        }
+    }
+
+    private val strokePaint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = strokeColor
+            style = Paint.Style.STROKE
+            strokeWidth = this@RoundProgressView.strokeWidth.toFloat()
+        }
+    }
+
+    private val progressRect = RectF()
 
     init {
         context.theme.obtainStyledAttributes(
             attrs,
             R.styleable.RoundProgressView,
-            0, 0).apply {
+            0, 0
+        ).apply {
 
             try {
-                progressWidth = getDimensionPixelSize(R.styleable.RoundProgressView_progressWidth, 10)
+                strokeWidth = getDimensionPixelSize(R.styleable.RoundProgressView_strokeWidth, 10)
                 progressColor = getColor(R.styleable.RoundProgressView_progressColor, progressColor)
-                centerColor = getColor(R.styleable.RoundProgressView_centerColor, centerColor)
+                strokeColor = getColor(R.styleable.RoundProgressView_strokeColor, strokeColor)
             } finally {
                 recycle()
             }
         }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
+        val w: Int = View.resolveSizeAndState(minw, widthMeasureSpec, 1)
+        val minh: Int = View.MeasureSpec.getSize(w) + paddingBottom + paddingTop
+        val h: Int = View.resolveSizeAndState(
+            View.MeasureSpec.getSize(w),
+            heightMeasureSpec,
+            0
+        )
+
+        val pad = strokeWidth.toFloat() / 2
+        progressRect.set(
+            pad + paddingLeft,
+            pad + paddingTop,
+            w.toFloat() - pad - paddingRight,
+            h.toFloat() - pad - paddingBottom
+        )
+        setMeasuredDimension(w, h)
+    }
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.run {
+            drawArc(progressRect, 0f, 360f, false, strokePaint)
+            drawArc(progressRect, 270f, -360f, false, progressPaint)
+        }
+
     }
 
 
