36323d3d492fbe1ec1c1c8e9d048d27496e0bacd
Junseok Oh <buddman1208@gmail.com>
2020-05-02 19:24:36 +0900

    [extensions] add : Observable<T>.clearAndSet

--- app/src/main/java/com/buddman1208/ecowell/utils/Extensions.kt
+++ app/src/main/java/com/buddman1208/ecowell/utils/Extensions.kt
@@ -1,8 +1,15 @@
 package com.buddman1208.ecowell.utils
 
 import android.content.res.Resources
+import androidx.databinding.ObservableField
 
 val Int.dp: Int
     get() = (this / Resources.getSystem().displayMetrics.density).toInt()
 val Int.px: Int
     get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+fun <T> ObservableField<T>.clearAndSet(value : T) {
+    set(null)
+    set(value)
+}
+
