9cac20c07c68faefc8c3c8d8f96edd123e97df2f
Junseok Oh <buddman1208@gmail.com>
2020-05-03 00:10:11 +0900

    [dialog/setting] connect to viewmodel

--- app/src/main/java/com/buddman1208/ecowell/ui/SettingDialogFragment.kt
+++ app/src/main/java/com/buddman1208/ecowell/ui/SettingDialogFragment.kt
@@ -1,5 +1,7 @@
 package com.buddman1208.ecowell.ui
 
+import android.os.Bundle
+import android.view.View
 import com.buddman1208.ecowell.R
 import com.buddman1208.ecowell.base.BaseDialogFragment
 import com.buddman1208.ecowell.databinding.DialogSettingBinding
@@ -10,5 +12,10 @@ class SettingDialogFragment :
     ) {
 
     override val viewModel: SettingDialogViewModel = SettingDialogViewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
+    }
 
 }
