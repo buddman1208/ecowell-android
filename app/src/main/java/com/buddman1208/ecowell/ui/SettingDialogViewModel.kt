49698439b5d7811f51cf75d26faa11ba46206647
Junseok Oh <buddman1208@gmail.com>
2020-05-03 16:11:01 +0900

    [dialog/setting] viewmodel : add variable settingType, add bindingAdapter to set textstyle by value

--- app/src/main/java/com/buddman1208/ecowell/ui/SettingDialogViewModel.kt
+++ app/src/main/java/com/buddman1208/ecowell/ui/SettingDialogViewModel.kt
@@ -1,5 +1,37 @@
 package com.buddman1208.ecowell.ui
 
+import android.widget.TextView
+import androidx.core.widget.TextViewCompat
+import androidx.databinding.BindingAdapter
+import androidx.databinding.ObservableInt
+import com.buddman1208.ecowell.R
 import com.buddman1208.ecowell.base.BaseViewModel
 
-class SettingDialogViewModel : BaseViewModel()
+class SettingDialogViewModel : BaseViewModel() {
+
+    val settingType: ObservableInt = ObservableInt(1)
+
+    fun selectType(value: Int) = settingType.set(value)
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter("selectedValue")
+        fun setTextStyleByValue(tv: TextView, value: Int) {
+            when (tv.id) {
+                R.id.tvType1 -> setButtonStyle(tv, (value == 1))
+                R.id.tvType2 -> setButtonStyle(tv, (value == 2))
+            }
+        }
+
+        private fun setButtonStyle(tv: TextView, isSelected: Boolean) {
+            TextViewCompat.setTextAppearance(
+                tv,
+                if (isSelected) R.style.SelectedTextView else R.style.NotSelectedTextView
+            )
+            tv.setBackgroundResource(
+                if (isSelected) R.drawable.bg_oval_accent
+                else R.drawable.bg_oval_gray
+            )
+        }
+    }
+}
