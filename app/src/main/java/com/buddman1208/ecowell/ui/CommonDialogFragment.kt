64cef3308f9e24c2f0065af18ad15ad778b1b51c
Junseok Oh <buddman1208@gmail.com>
2020-05-03 17:59:18 +0900

    [dialog/common] add common dialog components

---
+++ app/src/main/java/com/buddman1208/ecowell/ui/CommonDialogFragment.kt
@@ -0,0 +1,24 @@
+package com.buddman1208.ecowell.ui
+
+import android.os.Bundle
+import android.view.View
+import com.buddman1208.ecowell.R
+import com.buddman1208.ecowell.base.BaseDialogFragment
+import com.buddman1208.ecowell.databinding.DialogCommonBinding
+
+class CommonDialogFragment(
+    val dialogText : String
+) : BaseDialogFragment<DialogCommonBinding, CommonDialogViewModel>(
+    R.layout.dialog_common
+) {
+
+    override val viewModel: CommonDialogViewModel = CommonDialogViewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
+
+        viewModel.dialogContent.set(dialogText)
+    }
+}
+
---
+++ app/src/main/java/com/buddman1208/ecowell/ui/CommonDialogViewModel.kt
@@ -0,0 +1,10 @@
+package com.buddman1208.ecowell.ui
+
+import androidx.databinding.ObservableField
+import com.buddman1208.ecowell.base.BaseViewModel
+
+class CommonDialogViewModel : BaseViewModel() {
+
+    val dialogContent : ObservableField<String> = ObservableField("")
+
+}
