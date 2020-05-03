2f2b8c22ca873c7fc6fe22d1ca21ce20e5254c60
Junseok Oh <buddman1208@gmail.com>
2020-05-03 19:09:50 +0900

    [dialog/common] add positive, negative callback

--- app/src/main/java/com/buddman1208/ecowell/ui/commondialog/CommonDialogFragment.kt
+++ app/src/main/java/com/buddman1208/ecowell/ui/commondialog/CommonDialogFragment.kt
@@ -7,8 +7,10 @@ import com.buddman1208.ecowell.base.BaseDialogFragment
 import com.buddman1208.ecowell.databinding.DialogCommonBinding
 
 class CommonDialogFragment(
-    private val title : String = "",
-    private val text : String = ""
+    private val title: String = "",
+    private val text: String = "",
+    private val _positiveCallback: (() -> Unit)? = null,
+    private val _negativeCallback: (() -> Unit)? = null
 ) : BaseDialogFragment<DialogCommonBinding, CommonDialogViewModel>(
     R.layout.dialog_common
 ) {
@@ -26,6 +28,8 @@ class CommonDialogFragment(
         viewModel.run {
             dialogTitle.set(title)
             dialogContent.set(text)
+            positiveCallback = _positiveCallback
+            negativeCallback = _negativeCallback
         }
     }
 }
